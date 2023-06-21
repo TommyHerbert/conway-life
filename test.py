9db15a115e326b49c3d5f895da510204e972b70f
Tommy Herbert <tommyherbert@gmail.com>
2023-06-21 11:00:32 +0100

    Emptiness test

--- test.py
+++ test.py
@@ -6,5 +6,10 @@ class TestFourCellLifespans(unittest.TestCase):
         pattern_count = len(list(four_cell_lifespans.get_patterns(2, 2, 2)))
         self.assertEqual(6, pattern_count)
 
+    def test_emptiness(self):
+        empty = ((0,0,0), (0,0,0), (0,0,0))
+        self.assertTrue(four_cell_lifespans.empty_pattern())
+
+
 if __name__ == '__main__':
     unittest.main()
