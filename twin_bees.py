9e0b06ba70c6d034efbb4533753a73c903f3fcc6
Tommy Herbert <tommyherbert@gmail.com>
2023-06-27 20:53:43 +0100

    Add components of shuttle

--- twin_bees.py
+++ twin_bees.py
@@ -4,4 +4,11 @@
 import glife, golly
 
 block = glife.pattern('2o$2o!')
-block.display()
+b_heptomino = glife.pattern('o$2o$b2o$2o!')
+domino = glife.pattern('2o!')
+bee_with_domino = domino(0, 3) + b_heptomino(4, 0)
+bee_domino_block = bee_with_domino + block(14, 1)
+bees = bee_with_domino + bee_with_domino(0, 10, glife.flip_y)
+partial_shuttle = bees + block(14, 1)
+shuttle = partial_shuttle + block(-13, 1)
+shuttle.display()
