a383753c426355553f3c84c5562c5b1b1a1bea70
Tommy Herbert <tommyherbert@gmail.com>
2023-06-27 21:01:57 +0100

    Search code (still needs a definition for spiral)

--- twin_bees.py
+++ twin_bees.py
@@ -12,3 +12,18 @@ bees = bee_with_domino + bee_with_domino(0, 10, glife.flip_y)
 partial_shuttle = bees + block(14, 1)
 shuttle = partial_shuttle + block(-13, 1)
 shuttle.display()
+
+hat = pattern('2bo$bobo$bobo$2ob2o!')
+
+searching = True
+position = spiral(-13, 1)
+
+while searching:
+	x, y = next(position)
+	candidate = partial_shuttle + hat(x, y)
+	candidate.display()
+	hash_value = golly.hash(golly.getrect())
+	candidate.evolve(46)
+	candidate.display
+	if golly.hash(golly.getrect()) == hash_value:
+		searching = False
