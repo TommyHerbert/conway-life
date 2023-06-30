1c2cf67e98c5d578c8d22bc4a97d7f228f75ab15
Tommy Herbert <tommyherbert@gmail.com>
2023-06-30 09:09:19 +0100

    next_cell definition (WIP)

--- search.py
+++ search.py
@@ -1,11 +1,16 @@
 def spiral(start_x, start_y):
-	visited = [(start_x, start_y), (start_x, start_y - 1)]
-	for cell in visited:
-		yield cell
-	while True:
-		visited.append(next_cell(tuple(visited)))
-		yield visited[-1]
+    visited = [(start_x, start_y), (start_x, start_y - 1)]
+    for cell in visited:
+        yield cell
+    while True:
+        visited.append(next_cell(tuple(visited)))
+        yield visited[-1]
 
 
 def next_cell(visited):
-	XXXX
+    current = visited[-1]
+    vectors = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    north, east, south, west = [add_vector(current, v) for v in vectors]
+    if south in visited:
+        if east in visited:
+            
